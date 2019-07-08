1504f07a43aa0e2529dc4d2d6470a4222fe9b271
Stephanie Mattern <smattern416@gmail.com>
2019-07-08 13:31:20 -0400

    created friends.js

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,165 @@
+// ARRAY OF FRIENDS TO MATCH WITH
+var friends = [
+    {
+      "name": "Maura",
+      "photo": "https://cdn.evoke.ie/wp-content/uploads/2019/07/05141509/love_island_sr5_ep29_03-696x503.jpg",
+      "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      "name": "Curtis",
+      "photo": "https://i2-prod.cheshire-live.co.uk/incoming/article16549316.ece/ALTERNATES/s615/0_JS187378478.jpg",
+      "scores": [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      "name": "Amber",
+      "photo": "https://i0.wp.com/metro.co.uk/wp-content/uploads/2019/05/SEI_69829239-e1559476675827.jpg?quality=90&strip=all&zoom=1&resize=540%2C309&ssl=1",
+      "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      "name": "Georgia",
+      "photo": "https://cdn.evoke.ie/wp-content/uploads/2018/07/13120130/love-island-georgia1-696x503.jpg",
+      "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      "name": "Laura",
+      "photo": "https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/30/2018/06/Love-Island-Laura-Anderson1.jpg",
+      "scores": [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      "name": "Josh",
+      "photo": "https://i.dailymail.co.uk/i/newpix/2018/06/28/22/4DBD1A0000000578-0-image-a-13_1530222837003.jpg",
+      "scores": [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+      "name": "Jack",
+      "photo": "https://i2-prod.mirror.co.uk/incoming/article12725894.ece/ALTERNATES/s1200/Love-Island.jpg",
+      "scores": [
+        "3",
+        "2",
+        "4",
+        "2",
+        "2",
+        "4",
+        "4",
+        "3",
+        "3",
+        "2"
+      ]
+    },
+    {
+      "name": "Adam",
+      "photo": "https://video-images.vice.com/articles/5b2ceb79279b8900089860b3/lede/1529670734739-Screen-Shot-2018-06-22-at-133146.png",
+      "scores": [
+        "5",
+        "2",
+        "1",
+        "3",
+        "3",
+        "1",
+        "4",
+        "2",
+        "4",
+        "3"
+      ]
+    },
+    {
+      "name": "Anna",
+      "photo": "https://media.thetab.com/blogs.dir/90/files/2019/06/accessories-necklace-jewelry-accessory-arm-face-female-finger-apparel-clothing-person-human-940x480.jpg",
+      "scores": [
+        "5",
+        "2",
+        "1",
+        "3",
+        "3",
+        "5",
+        "4",
+        "2",
+        "4",
+        "3"
+      ]
+    },
+    {
+        "name": "Michael",
+        "photo": "https://i.dailymail.co.uk/1s/2019/07/03/13/15581704-7208113-image-a-23_1562156974657.jpg",
+        "scores": [
+          "5",
+          "2",
+          "1",
+          "3",
+          "3",
+          "5",
+          "4",
+          "2",
+          "4",
+          "3"
+        ]
+      }
+  ]
+  // MAKES ARRAY AVAILABLE TO USE IN OTHER FILES
+  module.exports = friends;
