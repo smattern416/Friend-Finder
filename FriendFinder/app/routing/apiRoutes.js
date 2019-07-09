e06352212c57baadb97eb8d049b67dd1ddbc2174
Stephanie Mattern <smattern416@gmail.com>
2019-07-09 09:53:38 -0400

    added app.post to routes

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -7,5 +7,10 @@ module.exports = function (app) {
  
   app.get("/api/friends", function (req, res) {
     res.json(friendData);
-  });
-}
+  })
+  app.post('/api/friends', function (req, res, next) {
+    console.log(req.body)
+    res.json(friendData)
+  })
+  
+};
